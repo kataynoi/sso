03e14904d507de92b435225729a0c474ba095c6c
kataynoi <thait-rex@hotmail.com>
2020-06-02 10:34:14 +0700

    bug1

--- application/views/base_data/policy.php
+++ application/views/base_data/policy.php
@@ -5,8 +5,14 @@
     <div class="panel-body">
         <?php
         echo $policy->name;
-        if($policy->file !=''){
-            echo "<span class='center'><img src='".base_url('assets/uploads/').$policy->file."' width='100%'></span>";
+        if (!empty($policy->file)) {
+            $type = explode(".", $policy->file);
+            if ($type[1] == 'jpg' || $type[1] == 'jpeg' || $type[1] == 'png' || $type[1] == 'gif') {
+                echo "<span class='center'><img src='" . base_url('assets/uploads/') . $policy->file . "' width='100%'></span>";
+
+            }elseif($type[1] == 'xlsx' || $type[1] == 'pdf' || $type[1] == 'xls' || $type[1] == 'doc' || $type[1] == 'docx'){
+                header("refresh:1;url=".base_url('assets/uploads/').$policy->file ."" );
+            }
         }
         ?>
     </div>
